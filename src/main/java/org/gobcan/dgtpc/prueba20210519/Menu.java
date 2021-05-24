7c3937ad88cb4563a2c1598ded8e2c4c77e8dbee
Albérica García <alberica@gmail.com>
2021-05-24 10:15:36 +0100

    Nueva versión del día 24 de mayo donde se incorpora una clase nueva

---
+++ src/main/java/org/gobcan/dgtpc/prueba20210519/Menu.java
@@ -0,0 +1,11 @@
+package org.gobcan.dgtpc.prueba20210519;
+
+public class Menu {
+	public static void main (String[] args) {
+		int elemento1 = 100;
+		int elemento2= 12;
+		System.out.println("La suma es " + Prueba20210519.suma(elemento1, elemento2));
+		System.out.println("La resta es " + Operaciones.resta(elemento1, elemento2));
+		}
+
+}
